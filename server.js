9c30ec82643dc7cfe93ea580cd043ef14257af79
[shuwei] <zhang_s1@gwu.edu>
2019-12-12 14:35:25 -0800

    Fix bugs

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ var proxy = httpProxy.createProxyServer({});
 const express = require('express');
 let app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // 3001
 app.get('/product/:pid', (req,res,next) => {
@@ -21,13 +19,13 @@ app.get('/product/:pid', (req,res,next) => {
 //3002
 app.get('/images/:pid', (req,res,next) => {
   let pid = req.params.pid;
-  proxy.web(req,res, {target:`http://ec2-18-188-250-254.us-east-2.compute.amazonaws.com:3002/app.js`});
+  proxy.web(req,res, {target:`http://ec2-18-188-250-254.us-east-2.compute.amazonaws.com:3002/`});
 });
 
 //3003
 app.get('/reviews/:pid', (req,res,next) => {
   let pid = req.params.pid;
-  proxy.web(req,res, {target:`http://ec2-13-58-212-164.us-east-2.compute.amazonaws.com:3003/bundle.js`});
+  proxy.web(req,res, {target:`http://ec2-13-58-212-164.us-east-2.compute.amazonaws.com:3003/`});
 });
 
 
