93da1a21e2c1c218fda08007bcc8d9e8f62533e9
[shuwei] <zhang_s1@gwu.edu>
2019-12-12 13:30:45 -0800

    Preparing for AWS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer(options);
 const express = require('express');
 let app = express();
 const path = require('path');
@@ -10,6 +11,25 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// 3001
+app.get('/product/:pid', (req,res,next) => {
+  let pid = req.params.pid;
+  proxy.web(req,res, {target:`http://ec2-13-58-162-184.us-east-2.compute.amazonaws.com:3001/`});
+});
+
+//3002
+app.get('/images/:pid', (req,res,next) => {
+  let pid = req.params.pid;
+  proxy.web(req,res, {target:`http://ec2-18-188-250-254.us-east-2.compute.amazonaws.com:3002/app.js`});
+});
+
+//3003
+app.get('/reviews/:pid', (req,res,next) => {
+  let pid = req.params.pid;
+  proxy.web(req,res, {target:`http://ec2-13-58-212-164.us-east-2.compute.amazonaws.com:3003/bundle.js`});
+});
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
