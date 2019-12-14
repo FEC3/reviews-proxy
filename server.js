6b376392598a0a2d08bf1cdfd970b49a4c816548
[shuwei] <zhang_s1@gwu.edu>
2019-12-13 23:16:02 -0800

    Testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 let app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT||3000;
 
 
 app.use(express.static('public'));
